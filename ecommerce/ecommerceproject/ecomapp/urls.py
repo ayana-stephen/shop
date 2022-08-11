a2ba42453cf6dcd31f42ad123628e68361bfbebc
ayana <ayanastephen123@gmail.com>
2022-08-11 15:13:01 +0530

    added

---
+++ ecommerce/ecommerceproject/ecomapp/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from .import views
+app_name='ecomapp'
+
+urlpatterns = [
+
+    path('',views.allProCat,name='allProCat'),
+    path('<slug:c_slug>/',views.allProCat,name='product_by_category'),
+    path('<slug:c_slug>/<slug:product_slug>/',views.proDetail,name='prodCatdetails')
+]
